3a560b716d1b8b1b34fd4f28555bf262ad2361bf
fkovacs12 <yepense@gmail.com>
2019-01-31 10:50:29 +0100

    second commit

--- finalbp/urls.py
+++ finalbp/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+
 
 urlpatterns = [
     url('admin/', admin.site.urls),
-    url(r'^$',views.homepage),
-    url(r'^kapcsolat/$',views.contact),
+    url(r'^$',views.homepage, name="fooldal"),
+    url(r'^kapcsolat/$',views.contact, name='kapcsolat'),
+    url(r'^szerviz/$',views.szerviz, name="szerviz"),
+    url(r'^ujautok/$',views.ujautok, name="ujautok"),
+
 
 ]
