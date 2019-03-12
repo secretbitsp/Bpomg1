7810de6090fe5a8f36a6baca5f4a5fbd9d891cc6
fkovacs12 <yepense@gmail.com>
2019-03-12 09:10:03 +0100

    ss2

--- sendemail/forms.py
+++ sendemail/forms.py
@@ -3,12 +3,12 @@ from django import forms
 
 class ContactForm(forms.Form):
     Név = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    Email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    Üzenet = forms.EmailField(required=True, widget=forms.Textarea(attrs={'class' : 'form-control'}))
+    Email = forms.EmailField( widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    Üzenet = forms.EmailField( widget=forms.Textarea(attrs={'class' : 'form-control'}))
 
 
 class AjanlatKeres(forms.Form):
     Név = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control'}))
     Telefonszám = forms.CharField(max_length=12)
-    Email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    Email = forms.EmailField( widget=forms.TextInput(attrs={'class' : 'form-control'}))
     Üzenet = forms.EmailField(required=True, widget=forms.Textarea(attrs={'class' : 'form-control'}))
