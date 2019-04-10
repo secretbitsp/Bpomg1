1609cb23b2469d94acbe88b6804ad2296f15b344
fkovacs12 <yepense@gmail.com>
2019-04-10 17:40:33 +0200

    ss

--- finalbp/settings.py
+++ finalbp/settings.py
@@ -130,7 +130,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
+STATICFILES_DIRS = (
+     os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = '/static/'
+
+STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
