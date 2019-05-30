520af260ad541b998b52bdf32e82b3be3166799e
fkovacs12 <yepense@gmail.com>
2019-05-30 17:49:03 +0200

    swr

--- bpcore/migrations/0003_post1.py
+++ bpcore/migrations/0003_post1.py
@@ -16,7 +16,6 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=120)),
                 ('description', models.TextField(max_length=250, null=True)),
-                ('content', tinymce.models.HTMLField(verbose_name='Content')),
                 ('draft', models.BooleanField(default=False)),
             ],
         ),
