e04971a1f2a55e56b4222f570241b8cf10071290
secretbitsp <ronakmundra123@gmail.com>
2020-03-06 22:13:01 +0530

    additional filters

--- static/js/filters.js
+++ static/js/filters.js
@@ -4,6 +4,7 @@
     var makeValue = $('#makes').val();
     var path = window.location.pathname;
     path += '?make=' + makeValue;
+    console.log(path);
     window.location.href = path;
   });
   $('#models').change(function(){
@@ -20,6 +21,12 @@
     var fuelValue = $('#fuels').val();
     var dateValue = $('#dates').val();
     var priceValue = $('#prices').val();
+    var category = $('#categories').val();
+    var mileage = $('#mileages').val();
+    var transmission = $('#transmissions').val();
+    console.log(transmission)
+    if(transmission == 'None') transmission = ""
+    console.log(transmission)
 
     var path = window.location.pathname;
     path += '?'
@@ -45,6 +52,21 @@
       path += path.endsWith('?') ? 'price=' : '&price=';
       path += priceValue;
     }
+    if (category) {
+
+      path += path.endsWith('?') ? 'category=' : '&category=';
+      path += category;
+    }
+    if (mileage) {
+
+      path += path.endsWith('?') ? 'mileage=' : '&mileage=';
+      path += mileage;
+    }
+    if (transmission) {
+
+      path += path.endsWith('?') ? 'transmission=' : '&transmission=';
+      path += transmission;
+    }
   //  path += '?make=' + makeValue + '&fuel=' + fuelValue + '&model=' + modelValue ;
     window.location.href = path;
 
@@ -63,3 +85,40 @@
 
     filterCars();
   });
+  $("#categories").change(function(){
+    filterCars();
+  })
+  $("#mileages").change(function(){
+    filterCars();
+  })
+  $("#transmissions").change(function(){
+    filterCars();
+  })
+
+var urlParams = new URLSearchParams(window.location.search);
+
+$(document).ready(function() {
+  var urlParams = new URLSearchParams(window.location.search);
+  var keys = urlParams.keys();
+  for(key of keys) { 
+    console.log(key);
+    console.log(urlParams.get(key));
+    if(key == 'category')
+      $('#categories').val(urlParams.get(key));
+    else if(key == 'make')
+      $('#makes').val(urlParams.get(key));
+    else if(key == 'mileage')
+      $('#mileages').val(urlParams.get(key));
+    else if(key == 'transmission')
+      $('#transmissions').val(urlParams.get(key));
+    else if(key == 'model')
+      $('#models').val(urlParams.get(key));
+    else if(key == 'fuel')
+      $('#fuels').val(urlParams.get(key));
+    else if(key == 'date')
+      $('#dates').val(urlParams.get(key));
+    console.log()
+  }
+});
+
+
